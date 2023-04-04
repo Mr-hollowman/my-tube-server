d78821168c65248d5d963798a7347dd26a594ca9
mr-hollowman <selvamani825@gmail.com>
2023-04-04 11:13:13 +0530

    create user and register user added with jwt

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import userRoutes from './routes/users.js';
+import videoRoutes from './routes/video.js';
+import commentRoutes from './routes/comments.js';
+import authRoutes from './routes/auth.js';
+import cookieParser from "cookie-parser";
 
 const app = express();
+dotenv.config()
 
+const connect = () => {
+    mongoose.connect(process.env.MONGO).then(() => {
+        console.log("connected to DB");
+    }).catch((err) => {
+        throw err;
+    })
+}
 
-app.listen(8080,()=>{
+app.use(cookieParser())
+app.use(express.json())
+
+app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/videos', videoRoutes)
+app.use('/api/comments', commentRoutes)
+
+app.use((err, req, res, next)=>{
+    const status = err.status || 500
+    const message = err.message ||"Something went wrong!";
+    return res.status(status).json({
+        success:false,
+        status,
+        message,
+    })
+})
+
+app.listen(8080, () => {
+    connect()
     console.log(`your app is running on http://localhost:8080`);
 })
