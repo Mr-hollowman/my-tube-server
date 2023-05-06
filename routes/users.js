cd83ceaad4a72f3045e7e1e62c267891d5070c79
mr-hollowman <selvamani825@gmal.com>
2023-05-06 19:53:08 +0530

    user controller added

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,28 @@
 import express from 'express';
-import { test } from '../controllers/user.js';
+import { deleteUser, dislike, getUser, like, subscribe, unSubscribe, update } from '../controllers/user.js';
 
 const router = express.Router();
 
-router.get('/test', test);
+//update user
+router.put("/:id",update)
+
+//delete user
+router.delete("/:id",deleteUser)
+
+//get a user
+router.get("/find/:id",getUser)
+
+//subscribe a user
+router.put("/sub/:id",subscribe)
+
+//unsubscribe a user
+router.put("/sub/:id",unSubscribe)
+
+//like a video
+router.put("/like/:videoId",like)
+
+//dislike a video
+router.put("/dislike/:videoId",dislike)
+
 
 export default router;
