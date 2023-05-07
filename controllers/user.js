e7172380988cf2c9e82fcd6eb52545821f215c09
mr-hollowman <selvamani825@gmal.com>
2023-05-07 18:18:20 +0530

    get user function added

--- controllers/user.js
+++ controllers/user.js
@@ -33,7 +33,13 @@ export const deleteUser = async (req, res, next) => {
     }
 }
 
-export const getUser = (req, res, next) => {
+export const getUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id)
+        res.status(200).json(user)
+    } catch (error) {
+        next(error)
+    }
 }
 
 export const subscribe = (req, res, next) => {
