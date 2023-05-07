eb09c121e1f00ef758df8f38184a11fc03d97acc
mr-hollowman <selvamani825@gmal.com>
2023-05-07 18:51:48 +0530

    delete video function added

--- controllers/video.js
+++ controllers/video.js
@@ -1,5 +1,5 @@
-import { createError } from "../error"
-import Video from "../models/Video"
+import { createError } from "../error.js"
+import Video from "../models/Video.js"
 
 export const addVideo = async (req, res, next) => {
     const newVideo = new Video({ userId: req.user.id, ...req.body })
@@ -30,7 +30,19 @@ export const updateVideo = async (req, res, next) => {
 }
 
 export const deleteVideo = async (req, res, next) => {
-
+    try {
+        const video = Video.findById(req.params.id)
+        if (!video) return next(createError(404, "video not found!"))
+        if (req.user.id === video.id) {
+            await Video.findByIdAndDelete(req.params.id)
+        }
+        else {
+            return next(createError(403, "you can delete only your video!"))
+        }
+        res.status(200).json("video deleted successfully")
+    } catch (error) {
+        next(error)
+    }
 }
 
 export const getVideo = async (req, res, next) => {
