7a70c728e1dd9914b3787b07f63be45c90d0bfa9
mr-hollowman <selvamani825@gmal.com>
2023-05-07 18:54:31 +0530

    get video function added

--- controllers/video.js
+++ controllers/video.js
@@ -46,5 +46,10 @@ export const deleteVideo = async (req, res, next) => {
 }
 
 export const getVideo = async (req, res, next) => {
-
+    try {
+        const video = await Video.findById(req.params.id)
+        res.status(200).json(video)
+    } catch (error) {
+        next(error)
+    }
 }
