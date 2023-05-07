1a7d0a523f0e8d82587ece0b8ac85916628ac874
mr-hollowman <selvamani825@gmal.com>
2023-05-07 18:07:42 +0530

    delete user function added

--- controllers/user.js
+++ controllers/user.js
@@ -1,37 +1,49 @@
 import { createError } from "../error.js"
 import User from "../models/User.js"
 
-export const update = async (req, res, next) =>{
-    if(req.params.id === req.user.id){
-        try{
+export const update = async (req, res, next) => {
+    if (req.params.id === req.user.id) {
+        try {
             const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-                $set:req.body
-            },{new:true})
+                $set: req.body
+            }, { new: true })
             res.status(200).json(updatedUser)
         }
-        catch(err){
+        catch (err) {
             next(err)
         }
     }
-    else{
-        next(createError(403,"you can update only your account"))
+    else {
+        next(createError(403, "you can update only your account"))
     }
 }
 
-export const deleteUser = (req, res, next) =>{
+export const deleteUser = async (req, res, next) => {
+    if (req.params.id === req.user.id) {
+        try {
+            await User.findByIdAndDelete(req.params.id)
+            res.status(200).json("user deleted successfully")
+        }
+        catch (err) {
+            next(err)
+        }
+    }
+    else {
+        next(createError(403, "you can update only your account"))
+    }
 }
 
-export const getUser = (req, res, next) =>{
+export const getUser = (req, res, next) => {
 }
 
-export const subscribe = (req, res, next) =>{
+export const subscribe = (req, res, next) => {
 }
 
-export const unSubscribe = (req, res, next) =>{
+export const unSubscribe = (req, res, next) => {
 }
 
-export const like = (req, res, next) =>{
+export const like = (req, res, next) => {
 }
 
-export const dislike = (req, res, next) =>{
+export const dislike = (req, res, next) => {
 }
