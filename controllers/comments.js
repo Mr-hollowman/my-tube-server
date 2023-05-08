f3172f2e904723d76e44e0ed72fddd2752e31656
mr-hollowman <selvamani825@gmal.com>
2023-05-08 16:43:08 +0530

    function added for delete comment

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,17 +1,27 @@
 import Comment from "../models/Comment.js";
+import Video from "../models/Video.js";
+import createError from "../error.js";
 
 export const addComment = async (req, res, next) => {
   const newComment = new Comment({ ...req.body, userId: req.user.id });
   try {
-    const savedComment = await newComment.save()
-    res.status(200).send(savedComment)
+    const savedComment = await newComment.save();
+    res.status(200).send(savedComment);
   } catch (error) {
     next(error);
   }
 };
 
 export const deleteComment = async (req, res, next) => {
+  const comment = new Comment.findById(req.params.id);
+  const video = new Video.findById(req.params.id);
   try {
+    if (req.user.id === comment.userId || req.user.id === video.userId) {
+      await Comment.findByIdAndDelete(req.params.id);
+      res.status(200).json("comment deleted successfully");
+    } else {
+      next(createError(403, "you don't have a access"));
+    }
   } catch (error) {
     next(error);
   }
