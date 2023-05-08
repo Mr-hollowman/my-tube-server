792abc584bbc50aea18ef997eed1f2788ff5e6f6
mr-hollowman <selvamani825@gmal.com>
2023-05-08 17:18:36 +0530

    function added for like a video

--- controllers/user.js
+++ controllers/user.js
@@ -1,77 +1,86 @@
-import { createError } from "../error.js"
-import User from "../models/User.js"
+import { createError } from "../error.js";
+import User from "../models/User.js";
+import Video from "../models/Video.js";
 
 export const update = async (req, res, next) => {
-    if (req.params.id === req.user.id) {
-        try {
-            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-                $set: req.body
-            }, { new: true })
-            res.status(200).json(updatedUser)
-        }
-        catch (err) {
-            next(err)
-        }
+  if (req.params.id === req.user.id) {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      next(err);
     }
-    else {
-        next(createError(403, "you can update only your account"))
-    }
-}
+  } else {
+    next(createError(403, "you can update only your account"));
+  }
+};
 
 export const deleteUser = async (req, res, next) => {
-    if (req.params.id === req.user.id) {
-        try {
-            await User.findByIdAndDelete(req.params.id)
-            res.status(200).json("user deleted successfully")
-        }
-        catch (err) {
-            next(err)
-        }
+  if (req.params.id === req.user.id) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("user deleted successfully");
+    } catch (err) {
+      next(err);
     }
-    else {
-        next(createError(403, "you can update only your account"))
-    }
-}
+  } else {
+    next(createError(403, "you can update only your account"));
+  }
+};
 
 export const getUser = async (req, res, next) => {
-    try {
-        const user = await User.findById(req.params.id)
-        res.status(200).json(user)
-    } catch (error) {
-        next(error)
-    }
-}
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const subscribe = async (req, res, next) => {
-    try {
-        await User.findByIdAndUpdate(req.user.id, {
-            $push: { subscribedUsers: req.params.id }
-        })
-        await User.findByIdAndUpdate(req.params.id, {
-            $inc: { subscribers: 1 }
-        })
-        res.status(200).json("subscribed successfully")
-    } catch (error) {
-        next(error)
-    }
-}
+  try {
+    await User.findByIdAndUpdate(req.user.id, {
+      $push: { subscribedUsers: req.params.id },
+    });
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: 1 },
+    });
+    res.status(200).json("subscribed successfully");
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const unSubscribe = async (req, res, next) => {
-    try {
-        await User.findByIdAndUpdate(req.user.id, {
-            $pull: { subscribedUsers: req.params.id }
-        })
-        await User.findByIdAndUpdate(req.params.id, {
-            $inc: { subscribers: -1 }
-        })
-        res.status(200).json("unsubscribed successfully")
-    } catch (error) {
-        next(error)
-    }
-}
+  try {
+    await User.findByIdAndUpdate(req.user.id, {
+      $pull: { subscribedUsers: req.params.id },
+    });
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: -1 },
+    });
+    res.status(200).json("unsubscribed successfully");
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const like = (req, res, next) => {
-}
-
-export const dislike = (req, res, next) => {
-}
+export const like = async (req, res, next) => {
+  const id = req.user.id;
+  const videoId = req.params.videoId;
+  try {
+    await Video.findByIdAndUpdate(videoId, {
+      $addToSet: { likes: id },
+      $pull: { dislikes: id },
+    });
+    res.status(200).json("The video has been liked.");
+  } catch (error) {
+    next(error);
+  }
+};
