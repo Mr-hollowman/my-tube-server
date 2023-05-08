8dac702ffb364197cb49ba6e9bed2e4c25f1566c
mr-hollowman <selvamani825@gmal.com>
2023-05-08 13:03:50 +0530

    comment routes added

--- routes/comments.js
+++ routes/comments.js
@@ -1,7 +1,11 @@
-import express from 'express'
-
-import {} from '../controllers/comments.js';
+import express from "express";
+import { verifyToken } from "../verifyToken.js";
+import { addComment, deleteComment } from "../controllers/comments.js";
 
 const router = express.Router();
+
+router.post("/", verifyToken, addComment);
+router.delete("/:id", verifyToken, deleteComment);
+router.get("/:videoID", deleteComment);
 
 export default router;
